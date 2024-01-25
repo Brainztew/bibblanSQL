3ea18edcc7a9f5acae20fb5ba000365796c88f5e
dennis <isacsson_89@hotmail.com>
2024-01-25 09:27:53 +0100

    all

---
+++ MOCK_DATA (2).sql
@@ -0,0 +1,34 @@
+create table MOCK_DATA (
+	book_title TEXT,
+	author VARCHAR(50),
+	publication_date DATE,
+	genre VARCHAR(15),
+	rating DECIMAL(2,1),
+	borrowed VARCHAR(50),
+	id VARCHAR(50)
+);
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('amet cursus id turpis integer', 'Mitch Lippett', '1957-12-14 00:20:24', 'science fiction', 2.3, true, '683134620-0');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('volutpat in congue etiam justo', 'Leticia Decker', '1929-12-01 09:51:12', 'mystery', 2.9, true, '000991572-9');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('ac neque duis bibendum', 'Phyllida Knappen', '1929-08-16 16:36:22', 'romance', 2.7, false, '207691114-6');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('vulputate nonummy', 'Carlee Donovin', '1914-08-25 22:19:36', 'mystery', 4.8, false, '563477531-0');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('interdum venenatis turpis enim', 'Ariel Hutfield', '1924-03-20 03:24:29', 'fiction', 1.9, false, '258621109-X');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('vitae ipsum aliquam non', 'Irvine Yashunin', '1940-11-19 00:47:46', 'romance', 3.5, true, '688271706-0');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('in lacus curabitur', 'Pammi Ianno', '1963-10-16 23:26:11', 'mystery', 2.6, true, '128370611-3');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('luctus rutrum', 'Pollyanna Eckert', '1932-01-06 19:30:47', 'science fiction', 3.7, false, '639955833-6');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('integer non velit donec', 'Walt Toller', '2004-06-30 16:28:20', 'science fiction', 1.4, true, '823078946-0');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('justo in hac habitasse', 'Miguel Battey', '1988-12-05 04:19:37', 'science fiction', 1.4, true, '353459554-8');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('ultrices aliquet maecenas', 'Reagan Taig', '1993-04-12 23:29:35', 'mystery', 1.2, false, '882199574-7');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('et ultrices', 'Olvan Reek', '1931-02-10 22:50:45', 'science fiction', 1.5, true, '053264439-5');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('sapien in sapien iaculis', 'Poppy Macklam', '1971-10-13 19:59:40', 'non-fiction', 3.7, false, '016702464-7');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('rutrum nulla tellus in sagittis', 'Bartolomeo Bigrigg', '2008-02-24 21:08:47', 'fiction', 3.4, false, '869689796-X');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('vehicula consequat morbi a', 'Birgitta Bartell', '2016-03-14 06:52:59', 'non-fiction', 4.2, false, '854673220-5');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('lobortis sapien sapien', 'Hetti McCumesky', '1929-08-01 18:08:02', 'science fiction', 4.2, true, '976270843-1');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('amet nunc viverra dapibus nulla', 'Calvin Maker', '1971-02-20 10:07:20', 'mystery', 4.2, true, '862395132-8');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('vel accumsan tellus', 'Adolpho O''Shiels', '1996-02-20 00:19:32', 'science fiction', 3.6, false, '088711603-5');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('integer non velit donec diam', 'Sanderson Rollingson', '1983-10-30 03:54:28', 'mystery', 1.5, true, '653713719-5');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('cubilia curae', 'Selia Wrout', '1916-11-03 14:27:08', 'fiction', 4.8, false, '475677929-8');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('est risus', 'Gussi Dingwall', '1946-06-06 07:49:11', 'science fiction', 1.3, false, '172028200-5');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('ligula suspendisse ornare consequat', 'Skyler Valasek', '1911-04-22 00:58:23', 'non-fiction', 2.1, false, '092504045-2');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('duis at velit eu est', 'Kimberley Craney', '1936-04-11 20:50:50', 'mystery', 3.1, true, '972308945-9');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('morbi vestibulum velit id pretium', 'Jay Plante', '1994-11-15 01:25:24', 'non-fiction', 3.9, true, '645510514-1');
+insert into MOCK_DATA (book_title, author, publication_date, genre, rating, borrowed, id) values ('ante ipsum primis in', 'Garrik Enrigo', '2013-02-24 13:17:10', 'mystery', 4.4, true, '571681355-0');
